4054f928b77a43760939d4b0f2b3afea53407c01
sayo-2123 <classa1990@gmail.com>
2024-05-06 17:40:38 +0900

    CSSの適用

---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,54 @@
+"use client"
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Post } from "@/interface";
+
+export const Article: React.FC<{ id: number, title: string, thumbnail: string, createdAt: string, categories: string, content: string }> = () => {
+  const [post, setPosts] = useState<Post | null>(null);
+  const { id } = useParams<{ id: string }>();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const fetcher = async () => {
+      const res = await fetch(
+        `https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts/${id}`
+      );
+      const { post } = await res.json();
+      setPosts(post);
+      setIsLoading(false);
+    };
+    fetcher();
+  }, [id]);
+
+  if (isLoading) {
+    return (
+      <div>読み込み中</div>
+    )
+  }
+
+  if (!post) {
+    return <div>記事が見つかりません</div>;
+  }
+
+  return (
+    <div key={post.id} className="postId">
+      <img src={post.thumbnailUrl} alt="Post thumbnail" />
+
+      <div className="postHead">
+        <div className="postCreatedAt">
+          {new Date(post.createdAt).toLocaleDateString("ja-JP")}
+        </div>
+        <div className="postCategories">
+          {post.categories.map((category, index) => (
+            <div key={index} className="postCategory">
+              {category}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="postTitle">{post.title}</div>
+      <div className="post">{post.content}</div>
+    </div>
+  );
+};
