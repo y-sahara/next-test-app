c50158c9b6cfdd7c5956a976d7db757bdc09c8a2
y.sahara01 <classa1990@gmail.com>
2024-08-27 00:43:26 +0900

    loginページの作成

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -58,7 +58,7 @@ export default function Page() {
             id="password"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 block w-full p-2.5"
             required
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) => setPassword(e.target.value)}
             value={password}
           />
         </div>
